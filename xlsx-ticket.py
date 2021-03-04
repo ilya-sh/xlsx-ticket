d6746eeb6a40b68cb5e3f1a42d4419bd6cb8135c
Ilya Shabanov <shabanoff.ilya@gmail.com>
2021-03-04 11:38:15 +0700

    Add files via upload

---
+++ xlsx-ticket.py
@@ -0,0 +1,35 @@
+"""
+This script converts Microsoft Excel Open XML Spreadsheet (.xlsx) file to tab separated value file (.tsv)
+that can be used in Jira to import projects and issues.
+
+Date format in the generated file is (yyyy-MM-dd hh:mm:ss) and have to be specified in project mappings through
+External System Import.
+
+See more at https://support.atlassian.com/jira-cloud-administration/docs/import-data-from-a-csv-file/
+"""
+
+import pandas as pd
+import json
+import sys
+
+try:
+    df = pd.read_excel(sys.argv[1])
+except OSError:
+    print(f'Incorrect data reading from {0}', sys.argv[1])
+    sys.exit(1)
+# df = pd.read_excel('test/exportSD.xlsx')
+
+with open("config.json", "r", encoding='utf-8') as config:
+    config_map = json.load(config)
+with open("dt_columns.json", "r", encoding='utf-8') as date:
+    dt_columns = json.load(date)
+
+for key in dt_columns:
+    df[key] = df[key].dt.round('1s')
+
+for key in config_map.keys():
+    df[key] = df[key].str.replace('\n', ' ')
+    df[key] = df[key].str.replace('\t', '')
+    df = df.replace({key: config_map[key]})
+
+df.to_csv(sys.argv[1].replace('.xlsx', '.csv'), sep='\t')
